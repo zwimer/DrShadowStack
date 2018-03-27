e102a4f0b0fbc9e266f8be40700b8c772695ae3d
zwimer <zwimer@gmail.com>
2018-03-27 19:18:33 +0000

    Made signals into c from cpp

--- toy-examples/signals.c
+++ toy-examples/signals.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 #include <signal.h>
 
-void s1(int) {
+void s1(int s) {
 	write(1, "+1\n", 3);
 	kill(getpid(), 3);
 	write(1, "-1\n", 3);
 }
-void s3(int) {
+void s3(int s) {
 	write(1, "+3\n", 3);
 	write(1, "-3\n", 3);
 }
