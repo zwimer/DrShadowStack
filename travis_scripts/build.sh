c1ae455f95eee245b3962f1cb319e481bc9c99f3
zwimer <zwimer@gmail.com>
2018-04-22 09:37:25 +0000

    Grab DR 7.0.0

--- travis_scripts/build.sh
+++ travis_scripts/build.sh
@@ -3,13 +3,15 @@ set -e
 cd $TRAVIS_BUILD_DIR
 
 # Get the latest release
-echo 'Locating latest DynamoRIO release...'
-mkdir dr && cd dr
-wget https://github.com/DynamoRIO/dynamorio/releases/latest -O - -nv > tmp
-LATEST=$(cat tmp | egrep '/.*/.*/.*tar\.gz' -o | grep x86)
+# echo 'Locating latest DynamoRIO release...'
+# mkdir dr && cd dr
+# wget https://github.com/DynamoRIO/dynamorio/releases/latest -O - -nv > tmp
+# LATEST=$(cat tmp | egrep '/.*/.*/.*tar\.gz' -o | grep x86)
 
-echo "Getting latest DynamoRIO release: $LATEST"
-wget https://github.com/$LATEST -nv
+# echo "Getting latest DynamoRIO release: $LATEST"
+# wget https://github.com/$LATEST -nv
+echo "Getting latest DynamoRIO 7.0.0"
+wget 'https://github.com/DynamoRIO/dynamorio/releases/download/release_7_0_0_rc1/DynamoRIO-Linux-7.0.0-RC1.tar.gz' -nv
 
 echo "Expanding it..."
 tar -zxf *.gz
