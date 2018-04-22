9b6631f03e1dde5532d37d3091edadc13d2c0836
zwimer <zwimer@gmail.com>
2018-04-22 09:28:16 +0000

    Download latest dr instead of building it

--- travis_scripts/build.sh
+++ travis_scripts/build.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 set -e
-
 cd $TRAVIS_BUILD_DIR
 
-git clone https://github.com/DynamoRIO/dynamorio.git | true
-cd dynamorio
+# Get the latest release
+echo 'Locating latest DynamoRIO release...'
+mkdir dr && cd dr
+wget https://github.com/DynamoRIO/dynamorio/releases/latest -O - -nv > tmp
+LATEST=$(cat tmp | egrep '/.*/.*/.*tar\.gz' -o | grep x86)
 
-if [ -d ./build ]; then
-	echo 'DynamoRIO cached'
-	cd build
-	DR=$(pwd)
-else
-	sudo apt-get install --yes cmake g++ g++-multilib doxygen transfig imagemagick ghostscript git
-	mkdir build | true
-	cd build
-	DR=$(pwd)
-	cmake ..
-	make -j 2
-fi
+echo "Getting latest DynamoRIO release: $LATEST"
+wget https://github.com/$LATEST -nv
 
+echo "Expanding it..."
+tar -zxf *.gz
+cd $(ls | grep -v 'tar')
+DR=$(pwd)
+
+echo "Building DrShadowStack..."
 cd $TRAVIS_BUILD_DIR
-mkdir src/build
-cd src/build
-
+mkdir src/build && cd src/build
 cmake .. -DDynamoRIO_DIR=$DR/cmake -DDRRUN_PATH=$DR/bin64/drrun
 make -j 2
