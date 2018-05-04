d4fe3f463aa3f7aa42cf1690b66ed4825c673ab5
zwimer <zwimer@gmail.com>
2018-05-04 11:42:13 +0000

    Test cases improved with example outputs

--- test-files/test-files/exec.c
+++ test-files/test-files/exec.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
 int main(int argc, char * argv[]) {
-	char * args[] = {"/usr/bin/stat", argv[0], 0};
+	char * args[] = {"/usr/bin/file", argv[0], 0};
 	execvp(args[0], args);
 }
